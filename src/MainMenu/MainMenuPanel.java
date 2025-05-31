d0e52f746ca49c76d2c6836e9172f62d2a0180ed
Jakubiou <jakubsramek2006@gmail.com>
2025-05-31 19:22:34 +0200

    Changed Main Menu Panel

--- src/MainMenu/MainMenuPanel.java
+++ src/MainMenu/MainMenuPanel.java
@@ -6,240 +6,133 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MainMenuPanel extends JFrame {
+    private Image backgroundImage;
+    private JButton playButton;
+    private JButton settingsButton;
+    private JButton quitGameButton;
+    private int originalButtonWidth;
+    private int originalButtonHeight;
 
     public MainMenuPanel() {
-        setSize(1600, 900);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = (int) screenSize.getWidth();
+        int screenHeight = (int) screenSize.getHeight();
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         ImageIcon backgroundIcon = new ImageIcon("res/watva/background/background1.png");
-        Image backgroundImage = backgroundIcon.getImage().getScaledInstance(1600, 850, Image.SCALE_SMOOTH);
-        backgroundIcon = new ImageIcon(backgroundImage);
+        backgroundImage = backgroundIcon.getImage().getScaledInstance(screenWidth, screenHeight, Image.SCALE_SMOOTH);
 
-        ImageIcon finalBackgroundIcon = backgroundIcon;
         JPanel backgroundPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.drawImage(finalBackgroundIcon.getImage(), 0, 0, null);
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
             }
         };
-        backgroundPanel.setLayout(null);
+        backgroundPanel.setLayout(new GridBagLayout());
         setContentPane(backgroundPanel);
 
-        ImageIcon playRPGIcon1 = new ImageIcon("res/buttons/PlayRPG_button1.png");
-        Image playRPGImage1 = playRPGIcon1.getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
-        playRPGIcon1 = new ImageIcon(playRPGImage1);
+        createButtons(screenWidth, screenHeight);
 
-        ImageIcon playRPGIcon2 = new ImageIcon("res/buttons/PlayRPG_button2.png");
-        Image playRPGImage2 = playRPGIcon2.getImage().getScaledInstance(320, 60, Image.SCALE_SMOOTH);
-        playRPGIcon2 = new ImageIcon(playRPGImage2);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(20, 0, 20, 0);
 
-        JButton playRPGButton = new JButton(playRPGIcon1);
+        backgroundPanel.add(playButton, gbc);
+        backgroundPanel.add(settingsButton, gbc);
+        backgroundPanel.add(quitGameButton, gbc);
 
-        playRPGButton.setBorderPainted(false);
-        playRPGButton.setContentAreaFilled(false);
-        playRPGButton.setFocusPainted(false);
-        playRPGButton.setOpaque(false);
+        addActionListeners();
 
-        ImageIcon playTDIcon1 = new ImageIcon("res/buttons/PlayTD_button1.png");
-        Image playTDImage1 = playTDIcon1.getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
-        playTDIcon1 = new ImageIcon(playTDImage1);
+        setVisible(true);
+    }
 
-        ImageIcon playTDIcon2 = new ImageIcon("res/buttons/PlayTD_button2.png");
-        Image playTDImage2 = playTDIcon2.getImage().getScaledInstance(320, 60, Image.SCALE_SMOOTH);
-        playTDIcon2 = new ImageIcon(playTDImage2);
+    private void createButtons(int screenWidth, int screenHeight) {
+        originalButtonWidth = (int) (screenWidth * 0.28);
+        originalButtonHeight = (int) (originalButtonWidth * 0.18);
 
-        JButton playTDButton = new JButton(playTDIcon1);
+        ImageIcon playIcon1 = new ImageIcon("res/buttons/Play_button1.png");
+        Image playImage1 = playIcon1.getImage().getScaledInstance(originalButtonWidth, originalButtonHeight, Image.SCALE_SMOOTH);
+        playIcon1 = new ImageIcon(playImage1);
 
-        playTDButton.setBorderPainted(false);
-        playTDButton.setContentAreaFilled(false);
-        playTDButton.setFocusPainted(false);
-        playTDButton.setOpaque(false);
+        ImageIcon playIcon2 = new ImageIcon("res/buttons/Play_button2.png");
+        Image playImage2 = playIcon2.getImage().getScaledInstance((int)(originalButtonWidth * 1.1), (int)(originalButtonHeight * 1.1), Image.SCALE_SMOOTH);
+        playIcon2 = new ImageIcon(playImage2);
+
+        playButton = new JButton(playIcon1);
+        styleButton(playButton, originalButtonWidth, originalButtonHeight);
 
         ImageIcon settingsIcon1 = new ImageIcon("res/buttons/Settings_button1.png");
-        Image settingsImage1 = settingsIcon1.getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
+        Image settingsImage1 = settingsIcon1.getImage().getScaledInstance(originalButtonWidth, originalButtonHeight, Image.SCALE_SMOOTH);
         settingsIcon1 = new ImageIcon(settingsImage1);
 
         ImageIcon settingsIcon2 = new ImageIcon("res/buttons/Settings_button2.png");
-        Image settingsImage2 = settingsIcon2.getImage().getScaledInstance(320, 60, Image.SCALE_SMOOTH);
+        Image settingsImage2 = settingsIcon2.getImage().getScaledInstance((int)(originalButtonWidth * 1.1), (int)(originalButtonHeight * 1.1), Image.SCALE_SMOOTH);
         settingsIcon2 = new ImageIcon(settingsImage2);
 
-        JButton settingsButton = new JButton(settingsIcon1);
-
-        settingsButton.setBorderPainted(false);
-        settingsButton.setContentAreaFilled(false);
-        settingsButton.setFocusPainted(false);
-        settingsButton.setOpaque(false);
+        settingsButton = new JButton(settingsIcon1);
+        styleButton(settingsButton, originalButtonWidth, originalButtonHeight);
 
         ImageIcon quitGameIcon1 = new ImageIcon("res/buttons/QuitGame_button1.png");
-        Image quitGameImage1 = quitGameIcon1.getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
+        Image quitGameImage1 = quitGameIcon1.getImage().getScaledInstance(originalButtonWidth, originalButtonHeight, Image.SCALE_SMOOTH);
         quitGameIcon1 = new ImageIcon(quitGameImage1);
 
         ImageIcon quitGameIcon2 = new ImageIcon("res/buttons/QuitGame_button2.png");
-        Image quitGameImage2 = quitGameIcon2.getImage().getScaledInstance(320, 60, Image.SCALE_SMOOTH);
+        Image quitGameImage2 = quitGameIcon2.getImage().getScaledInstance((int)(originalButtonWidth * 1.1), (int)(originalButtonHeight * 1.1), Image.SCALE_SMOOTH);
         quitGameIcon2 = new ImageIcon(quitGameImage2);
 
-        JButton quitGameButton = new JButton(quitGameIcon1);
+        quitGameButton = new JButton(quitGameIcon1);
+        styleButton(quitGameButton, originalButtonWidth, originalButtonHeight);
 
-        quitGameButton.setBorderPainted(false);
-        quitGameButton.setContentAreaFilled(false);
-        quitGameButton.setFocusPainted(false);
-        quitGameButton.setOpaque(false);
+        addHoverEffects(playButton, playIcon1, playIcon2);
+        addHoverEffects(settingsButton, settingsIcon1, settingsIcon2);
+        addHoverEffects(quitGameButton, quitGameIcon1, quitGameIcon2);
+    }
 
-        ImageIcon playRPGTDIcon1 = new ImageIcon("res/buttons/PlayRPGTD_button1.png");
-        Image playRPGTDImage1 = playRPGTDIcon1.getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
-        playRPGTDIcon1 = new ImageIcon(playRPGTDImage1);
+    private void styleButton(JButton button, int width, int height) {
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setOpaque(false);
+        button.setPreferredSize(new Dimension(width, height));
+    }
 
-        ImageIcon playRPGTDIcon2 = new ImageIcon("res/buttons/PlayRPGTD_button2.png");
-        Image playRPGTDImage2 = playRPGTDIcon2.getImage().getScaledInstance(320, 60, Image.SCALE_SMOOTH);
-        playRPGTDIcon2 = new ImageIcon(playRPGTDImage2);
-
-        JButton playRPGTDButton = new JButton(playRPGTDIcon1);
-
-        playRPGTDButton.setBorderPainted(false);
-        playRPGTDButton.setContentAreaFilled(false);
-        playRPGTDButton.setFocusPainted(false);
-        playRPGTDButton.setOpaque(false);
-
-        playRPGButton.setBounds(615, 150, 300, 50);
-        playTDButton.setBounds(615, 250, 300, 50);
-        playRPGTDButton.setBounds(615, 350, 300, 50);
-        settingsButton.setBounds(615, 450, 300, 50);
-        quitGameButton.setBounds(615, 550, 300, 50);
-
-        backgroundPanel.add(playRPGButton);
-        backgroundPanel.add(playTDButton);
-        backgroundPanel.add(playRPGTDButton);
-        backgroundPanel.add(settingsButton);
-        backgroundPanel.add(quitGameButton);
-
-        ImageIcon finalPlayRPGIcon = playRPGIcon2;
-        ImageIcon finalPlayRPGIcon1 = playRPGIcon1;
-        playRPGButton.addMouseListener(new MouseAdapter() {
+    private void addHoverEffects(JButton button, ImageIcon normalIcon, ImageIcon hoverIcon) {
+        button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                playRPGButton.setIcon(finalPlayRPGIcon);
-                playRPGButton.setBounds(605, 140, 320, 60);
+                button.setIcon(hoverIcon);
+                int newWidth = (int)(originalButtonWidth * 1.1);
+                int newHeight = (int)(originalButtonHeight * 1.1);
+                button.setPreferredSize(new Dimension(newWidth, newHeight));
+                button.revalidate();
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                playRPGButton.setIcon(finalPlayRPGIcon1);
-                playRPGButton.setBounds(615, 150, 300, 50);
+                button.setIcon(normalIcon);
+                button.setPreferredSize(new Dimension(originalButtonWidth, originalButtonHeight));
+                button.revalidate();
             }
         });
+    }
 
-        ImageIcon finalPlayTDIcon = playTDIcon2;
-        ImageIcon finalPlayTDIcon1 = playTDIcon1;
-        playTDButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playTDButton.setIcon(finalPlayTDIcon);
-                playTDButton.setBounds(605, 240, 320, 60);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                playTDButton.setIcon(finalPlayTDIcon1);
-                playTDButton.setBounds(615, 250, 300, 50);
-            }
+    private void addActionListeners() {
+        playButton.addActionListener(e -> {
+            new Game();
+            dispose();
         });
 
-        ImageIcon finalSettingsIcon = settingsIcon2;
-        ImageIcon finalSettingsIcon1 = settingsIcon1;
-        settingsButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                settingsButton.setIcon(finalSettingsIcon);
-                settingsButton.setBounds(605, 440, 320, 60);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                settingsButton.setIcon(finalSettingsIcon1);
-                settingsButton.setBounds(615, 450, 300, 50);
-            }
+        settingsButton.addActionListener(e -> {
+            System.out.println("Settings clicked");
         });
 
-        ImageIcon finalQuitGameIcon = quitGameIcon2;
-        ImageIcon finalQuitGameIcon1 = quitGameIcon1;
-        quitGameButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                quitGameButton.setIcon(finalQuitGameIcon);
-                quitGameButton.setBounds(605, 540, 320, 60);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                quitGameButton.setIcon(finalQuitGameIcon1);
-                quitGameButton.setBounds(615, 550, 300, 50);
-            }
-        });
-
-        ImageIcon finalPlayRPGTDIcon = playRPGTDIcon2;
-        ImageIcon finalPlayRPGTDIcon1 = playRPGTDIcon1;
-        playRPGTDButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playRPGTDButton.setIcon(finalPlayRPGTDIcon);
-                playRPGTDButton.setBounds(605, 340, 320, 60);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                playRPGTDButton.setIcon(finalPlayRPGTDIcon1);
-                playRPGTDButton.setBounds(615, 350, 300, 50);
-            }
-        });
-
-        setVisible(true);
-
-        playRPGButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Game();
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-                dispose();
-            }
-        });
-
-        playTDButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException ex) {
-                    throw new RuntimeException(ex);
-                }
-                dispose();
-            }
-        });
-
-        playRPGTDButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Play RPGTD clicked");
-            }
-        });
-
-        settingsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Settings clicked");
-            }
-        });
-
-        quitGameButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
+        quitGameButton.addActionListener(e -> {
+            dispose();
         });
     }
 }
