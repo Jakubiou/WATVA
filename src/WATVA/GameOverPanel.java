5c6b859fa439fd85e6aadd518bff19afd39c4858
Jakubiou <jakubsramek2006@gmail.com>
2025-05-31 16:02:30 +0200

    Added new pixel font

--- src/WATVA/GameOverPanel.java
+++ src/WATVA/GameOverPanel.java
@@ -4,42 +4,49 @@ import MainMenu.MainMenuPanel;
 
 import javax.swing.*;
 import java.awt.*;
-
+import java.io.FileInputStream;
 
 public class GameOverPanel extends JPanel {
-
     private Game game;
     private GamePanel gamePanel;
+    private Font pixelPurlFont;
 
     public GameOverPanel(Game game, GamePanel gamePanel) {
         this.game = game;
         this.gamePanel = gamePanel;
+        loadCustomFont();
         initializeComponents();
     }
 
+    private void loadCustomFont() {
+        try {
+            FileInputStream fontStream = new FileInputStream("res/fonts/PixelPurl.ttf");
+            pixelPurlFont = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(24f);
+        } catch (Exception e) {
+            pixelPurlFont = new Font("Arial", Font.BOLD, 24);
+            e.printStackTrace();
+        }
+    }
+
     private void initializeComponents() {
-        setLayout(new GridLayout(5, 1, 10, 10));
+        setLayout(new GridLayout(4, 1, 10, 10));
         setBackground(new Color(0, 0, 0, 150));
         setBounds((GamePanel.PANEL_WIDTH - 300) / 2, (GamePanel.PANEL_HEIGHT - 400) / 2, 300, 400);
 
         JLabel gameOverLabel = new JLabel("YOU DIED", JLabel.CENTER);
-        gameOverLabel.setFont(new Font("Arial", Font.BOLD, 36));
+        gameOverLabel.setFont(pixelPurlFont.deriveFont(36f));
         gameOverLabel.setForeground(Color.WHITE);
 
         JButton restartButton = createButton("Play again");
-        restartButton.addActionListener(e ->{
+        restartButton.addActionListener(e -> {
             gamePanel.restartGame();
         });
 
-        JButton upgradeButton = createButton("Upgrade");
+        JButton upgradeButton = createButton("Upgrades");
         upgradeButton.addActionListener(e -> {
             this.setVisible(false);
             gamePanel.initializeUpgradePanel();
         });
-
-
-        JButton levelsButton = createButton("Levels");
-        //levelsButton.addActionListener(e -> game.showLevelSelectPanel());
 
         JButton mainMenuButton = createButton("Main Menu");
         mainMenuButton.addActionListener(e -> {
@@ -50,13 +57,12 @@ public class GameOverPanel extends JPanel {
         add(gameOverLabel);
         add(restartButton);
         add(upgradeButton);
-        add(levelsButton);
         add(mainMenuButton);
     }
 
     private JButton createButton(String text) {
         JButton button = new JButton(text);
-        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setFont(pixelPurlFont);
         button.setForeground(Color.WHITE);
         button.setBackground(Color.DARK_GRAY);
         button.setFocusPainted(false);
